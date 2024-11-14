d6f5e63159aea505b39c67cf2df6bbdc30761c89
JulianUbu <thelastpocket@gmail.com>
2024-11-13 23:21:34 -0800

    Create repo and start on pub

---
+++ build/J-Point/CMakeFiles/J-Point_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/J-Point_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/J-Point_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/J-Point_pub/src/sense_pub.cpp
@@ -0,0 +1,7 @@
+#include "rclcpp/rclcpp.hpp"
+#include <librealsense2/rs.hpp> 
+#include "std_msgs/msg/string.hpp"
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+}
